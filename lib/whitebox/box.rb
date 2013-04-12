f78b5191167a1824f829d29a597bdd339d395114
Domas <domas@samesystem.com>
2013-04-12 19:03:46 +0300

    DSL starting point

--- lib/whitebox/box.rb
+++ lib/whitebox/box.rb
@@ -1,59 +1,65 @@
 module Whitebox
   class Box
-    attr_accessor :data, :toggle, :orders, :balance, :amount, :skip
+    attr_accessor :data, :toggle, :balance, :amount, :skip, :toggles
 
     def initialize
-      @orders ||= []
       @data = Securities::Stock.new(symbol: 'AAPL', start_date: '2012-01-01').output
     end
 
     def run
+      @balance = @amount = @skip = 0
+      @toggles = {}
+
+      Order.clear
       @data.each_with_index do |datapoint, i|
-        each_datapoint(datapoint, @data.first(i+1))
+        @current_data = @data.first(i+1)
+        each_datapoint(datapoint)
       end
-      @balance ||= 0
-      @amount ||= 0
-      @orders.each do |order|
-        cost = order.price * order.amount
-        if order.action == :open
-          @balance += cost
-          @amount += order.amount
-        elsif order.action == :close && @amount >= order.amount
-          @balance -= cost
-          @amount -= order.amount
-        else
-          puts "Skipped #{order.inspect}"
-          puts "because balance #{@amount} >= #{order.amount} && action = #{order.action}"
-        end
+      orders = Order.all
+
+      orders.each do |order|
+        @balance += order.datapoint[:close] * order.amount
+        @amount += order.amount
       end
+
       puts "Skip count: #{@skip}."
-      puts "Orders count: #{@orders.count}"
-      puts "Balance: #{@balance}"
+      puts "Orders count: #{orders.count}"
+      puts "Balance: #{@balance.round(2)}"
     end
 
-    def toggle(which)
-      @toggle ||= which # setting for the first time
-      @toggle != which and @toggle = which
-    end
+    def each_datapoint(datapoint)
 
-    def each_datapoint(datapoint, current_data)
-      # puts "Processing: #{datapoint[:date]}"
-      indicator_data = Indicators::Data.new(current_data)
-      sma_15 = indicator_data.calc(type: :sma, params: 15).output.last
-      sma_20 = indicator_data.calc(type: :sma, params: 20).output.last
-
-      if sma_15 > sma_20 && toggle(:over)
-        @orders << Order.new(:open, datapoint[:close].to_f, 1)
-        puts "#{sma_15} now over #{sma_20}"
-      elsif sma_15 < sma_20 && toggle(:under)
-        @orders << Order.new(:close, datapoint[:close].to_f, -1)
-        puts "#{sma_15} now under #{sma_20}"
+      if toggle { sma(15, 20) > sma(20) }
+        Order.make(datapoint, 1)
+        puts "#{sma(15, 20)} now over #{sma(20)}"
+      elsif toggle { sma(100) < sma(200) }
+        binding.pry
+        Order.make(datapoint, -1)
+        puts "#{sma(100)} now under #{sma(200)}"
       end
 
-    rescue
-      @skip ||= 0
-      @skip += 1 
-      puts "Skipping #{datapoint[:date]}"
+    rescue Indicators::Helper::HelperException
+      @skip += 1
     end
+
+    def method_missing(m, *args)
+      @indicator_data ||= {}
+      datakey = "#{@current_data.first[:date]} #{@current_data.last[:date]}"
+      @indicator_data[datakey] = Indicators::Data.new(@current_data) unless @indicator_data[datakey]
+
+      @indicator_data[datakey].calc(type: m, params: args).output.last
+    rescue => e
+      raise e
+    end
+
+    private
+
+    def toggle(&rule)
+      @toggles[rule.source_location] ||= rule.yield
+      result = @toggles[rule.source_location] != rule.yield
+      @toggles[rule.source_location] = rule.yield
+      result
+    end
+
   end
 end
--- lib/whitebox/order.rb
+++ lib/whitebox/order.rb
@@ -1,11 +1,31 @@
 module Whitebox
   class Order
-    attr_accessor :action, :price, :amount
-    def initialize(action, price, amount)
-      @action = action
+    attr_accessor :datapoint, :amount
+    @@orders = []
+
+    def initialize(datapoint, amount)
+      # correct formating
+      @datapoint = datapoint.each_with_object({}) do |(key, value), obj|
+        obj[key] = key == :date ? value : value.to_f
+      end
       @amount = amount
-      @price = price
-      return self
+    end
+
+    class << self
+
+      def make(datapoint, amount)
+        @@orders << new(datapoint, amount)
+        puts "Made an order at #{datapoint[:date]} @ #{datapoint[:close]} * #{amount}"
+      end
+
+      def clear
+        @@orders = []
+      end
+
+      def all
+        @@orders
+      end
+
     end
   end
 end
